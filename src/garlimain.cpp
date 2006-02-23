125f7d245a0e1b5516b4454d9d5578d6100119d8
nescent <nescent@f68b6e6a-deb8-11de-9f73-7335da764322>
2006-02-23 19:55:28 +0000

    Initial GARLI v0.94

---
+++ src/garlimain.cpp
@@ -0,0 +1,189 @@
+// GARLI version 0.93 source code
+// Copyright  2005 by Derrick J. Zwickl
+// All rights reserved.
+//
+// This code may be used and modified for non-commercial purposes
+// but redistribution in any form requires written permission.
+// Please contact:
+//
+//  Derrick Zwickl
+//	Integrative Biology, UT
+//	1 University Station, C0930
+//	Austin, TX  78712
+//  email: zwickl@mail.utexas.edu
+//
+//	Note: In 2006  moving to NESCENT (The National
+//	Evolutionary Synthesis Center) for a postdoc
+
+//	NOTE: Portions of this source adapted from GAML source, written by Paul O. Lewis
+
+
+#ifdef WINDOWS
+#include <conio.h>
+#endif
+
+#include "population.h"
+#include "individual.h"
+#include "parameters.h"
+#include "adaptation.h"
+#include "mlhky.h"
+#include "defs.h"
+#include "funcs.h"
+#include "mpifuncs.h"
+#include "hashdefines.h"
+#include "tree.h"
+#include "mlhky.h"
+#include "errorexception.h"
+
+//include Sioux console manipulators
+#ifdef WINDOWS
+#include "winsioux.h"
+#else
+#ifdef MAC
+#include "sioux.h"
+#endif
+#endif
+
+//allocation monitoring stuff from Paul, Mark and Dave
+#define WRITE_MEM_REPORT_TO_FILE
+#undef MONITORING_ALLOCATION
+#ifdef MONITORING_ALLOCATION
+	#define INSTANTIATE_MEMCHK
+#endif
+#include "memchk.h"
+
+#ifdef PROFILE
+#include "profiler.h"
+#endif
+
+#ifdef MPI_VERSION
+#include "mpi.h"
+#endif
+
+char programName[81];
+
+int main( int argc, char* argv[] )	{
+
+	#ifdef MAC
+	SIOUXSettings.columns = 90;
+	SIOUXSettings.rows = 40;
+	SIOUXSettings.asktosaveonclose = false;
+	SIOUXSettings.autocloseonquit = true;
+	SIOUXSettings.showstatusline = true;
+	SIOUXSetTitle("\pGARLI 0.93");
+	#endif
+
+
+	bool poo=true;
+//	while(poo);
+
+	CREATE_MEMCHK{
+	srand(time(NULL));
+	#ifdef MPI_VERSION
+		MPIMain(argc, argv);
+	#else
+	// init some stuff
+
+	char conf_name[20];
+	strcpy(conf_name, "garli.conf");
+
+    if (argc > 1) {
+    	int curarg=1;
+        while(curarg<argc){
+#ifdef GANESH 
+     		if(strlen(argv[curarg]) <= 2 && isdigit(argv[curarg][0])) {
+	        	Tree::p_value       = atoi(argv[curarg]);
+                if (Tree::p_value == 0) {
+                    Tree::random_p = true;
+                }
+//                else {
+  //                  Tree::ComputeRealCatalan();
+    //            }
+            }
+	      	else
+#endif
+	        	strcpy(conf_name, argv[curarg]);
+	        curarg++;
+			}
+		}
+#ifdef GANESH
+	if(Tree::random_p==false) Tree::ComputeRealCatalan();
+#endif
+
+		try{
+
+			cout << "Running serial GARLI, version 0.94 (Feb 06)" << endl;
+			MasterGamlConfig conf;
+			cout << "Reading config file " << conf_name << endl;
+			if (conf.Read(conf_name) < 0)	{
+				cout << "Error in config file...aborting." << endl;
+				return 0;
+			}
+
+			// Create the data object
+			HKYData data;
+			int err=ReadData(conf.datafname.c_str(), &data);
+			if(err==-1) return 0;
+			
+			// create the parameters object
+			Parameters params;
+			params.SetParams(conf, data);
+			
+			Tree::meanBrlenMuts	= params.meanBrlenMuts;
+			Tree::alpha		= params.gammaShapeBrlen;
+
+			// create the population object
+			Population pop;
+		
+			pop.Setup(params, &conf, 1, 0);
+
+			#ifdef PROFILE
+				ProfilerInit(collectDetailed, bestTimeBase, 100000, 1000);
+			#endif
+			if(pop.bootstrapReps == 0)
+			pop.Run();
+			else pop.Bootstrap();
+			}catch(ErrorException err){
+				err.Print(cout);
+				return 0;
+				}
+			catch(int error){
+				if(error==Population::nomem) cout << "not able to allocate enough memory!!!" << endl;
+				return 0;
+				}
+
+#ifndef UNIX
+	cout << "-Press enter to close program.-" << endl;
+	while( cin && (cin.get() != '\n') );
+	char d=getchar();
+#endif
+//	exit(0);
+
+
+#endif
+	}
+	#if defined(MONITORING_ALLOCATION) && !defined(NDEBUG)
+		#if defined(WRITE_MEM_REPORT_TO_FILE)
+			char filename[50];
+			#ifndef WIN32_VERSION
+			int rank=0;
+			MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+			sprintf(filename, "memecheck%d.txt", rank);
+			#else
+			strcpy(filename, "memcheck.txt");
+			#endif
+			ofstream memf(filename);
+			MEMCHK_REPORT(memf)
+			memf.close();
+		#else
+			MEMCHK_REPORT(cout)
+		#endif
+	#endif
+	#ifdef PROFILE
+	ProfilerDump("\phalfnewrescale.prof");
+	ProfilerTerm();
+	#endif
+
+	return 0;
+};
+
---
+++ src/subset.cpp
@@ -0,0 +1,107 @@
+// GARLI version 0.93 source code
+// Copyright  2005 by Derrick J. Zwickl
+// All rights reserved.
+//
+// This code may be used and modified for non-commercial purposes
+// but redistribution in any form requires written permission.
+// Please contact:
+//
+//  Derrick Zwickl
+//	Integrative Biology, UT
+//	1 University Station, C0930
+//	Austin, TX  78712
+//  email: zwickl@mail.utexas.edu
+//
+//	Note: In 2006  moving to NESCENT (The National
+//	Evolutionary Synthesis Center) for a postdoc
+
+#include <iostream>
+#include <cassert>
+
+using namespace std;
+
+#include "subset.h"
+
+// Added by Alan for subset SPR
+
+subset::subset(){
+  total = 0;
+  for(int i =0;i<1024;i++){front[i]=0;element[i]=0;}
+}
+void subset::setseed(int seed, double dist/*=0.0*/){
+  seednumber = seed;
+  total = 1;
+  element[0] = seed;
+  front[0] = 0;
+  pathlength[0]=dist;
+}
+void subset::addelement(int elenumber,int elemfront, double dist){
+  for(int i = 0;i<total;i++)
+    {
+      if(element[i]==elenumber) return;
+    }
+  total ++;
+  element[total-1] = elenumber;
+  front[total-1]   = elemfront;
+  pathlength[total-1] = dist;
+}
+
+void subset::setfront(int elenumber,int elemfront){
+  for(int i = 0;i<total;i++)
+    {
+      if(element[i]==elenumber)front[i] = elemfront;
+    }
+}
+int subset::getfront(int elenumber){
+  for(int i = 0; i<total;i++){
+    if(element[i]==elenumber) return(front[i]);
+  }
+  assert(0);
+  return 0;
+}
+
+void subset::elementremove(int elenumber){
+	 for(int i = 0; i<total;i++){
+	 	if(element[i]==elenumber){
+	 		element[i]=0;
+	 		break;
+	 		}
+	 	}
+	}
+
+void subset::removennis(){
+	//this just removes everything with a distance of 1
+	for(int i = 0; i<total;i++){
+ 		if(front[i]==1){
+ 			element[i]=0;
+ 			}
+		}
+	}
+
+void subset::compact(){
+	int offset=0;
+	for(int i=0;i<total;i++){
+		if(element[i+offset]==0 && (i+offset) < total)
+			while(element[i+offset]==0 && (i+offset) < total) offset++;
+		element[i]=element[i+offset];
+		//need to add these, dummy.
+		front[i]=front[i+offset];
+		pathlength[i]=pathlength[i+offset];
+		}
+	total -= offset;
+	} 
+void subset::clear(){
+  total = 0;
+  seednumber = -1;
+  for(int i = 0;i<1024;i++)
+    {  element[i]=0;
+    front[i] = 0;}
+}
+void subset::print(){
+  cout <<endl<<"  Print out for debug of subset SPRmutaion:"<<endl;
+  cout <<"   Total number of the range : "<< total << endl;
+  cout << "  Seed number is: "<< seednumber << endl;
+
+
+
+}
