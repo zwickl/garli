5049e82a912a96792f783e2d6e1e456ab52650ff
derrickzwickl <derrickzwickl@f68b6e6a-deb8-11de-9f73-7335da764322>
2008-02-12 20:49:17 +0000

    Much improved and more technically correct version of the mpi run splitter code

--- garli/trunk/src/mpitrick.cpp
+++ garli/trunk/src/mpitrick.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <stdlib.h>
 #include <stdio.h>
+#include "defs.h"
 #include "string.h"
 #include "mpi.h"
 #include <time.h>
@@ -13,32 +14,69 @@ void jobloop(int, int, MPI_Comm, int numJobs);
 
 int main(int argc,char **argv){
 
-  cout << "Garli started with command line:" << endl;
-  for(int i=0;i<argc;i++) cout << argv[i] << " ";
-  cout << endl;  
-
-  if(argc == 1 || !isdigit(argv[1][0])){
-	cout << "ERROR:\nGarli is expecting the number of jobs to be run to follow\nthe executable name on the command line" << endl;
-	return 1;
+  int rc = MPI_Init(&argc,&argv); 
+  if(rc != MPI_SUCCESS){
+	cout << "Error starting MPI.  Terminating." << endl;
+	MPI_Abort(MPI_COMM_WORLD, rc);
 	}
 
-  MPI_Comm comm,mycomm; int ntids,mytid;
-  MPI_Init(&argc,&argv); comm = MPI_COMM_WORLD;
-  MPI_Comm_size(comm,&ntids); MPI_Comm_rank(comm,&mytid);
-  MPI_Comm_split(comm,mytid,mytid,&mycomm);
+  MPI_Comm comm,mycomm; 
+  int nproc, rank;
+  comm = MPI_COMM_WORLD;
+  MPI_Comm_size(comm,&nproc); 
+  MPI_Comm_rank(comm,&rank);
 
-  int numJobs = atoi(argv[1]);
+//  MPI_Comm_split(comm,rank,rank,&mycomm);
+  int numJobsTotal = 0;
+  
+  if(rank == 0){
+  	cout << "Garli started with command line:" << endl;
+	for(int i=0;i<argc;i++) cout << argv[i] << " ";
+	cout << endl;  
 
-  jobloop(mytid,ntids,mycomm,numJobs);
-	
+	if(argc == 1 || (argv[1][0] != '-' && !isdigit(argv[1][0]))){
+		cout << "ERROR:\nGarli is expecting the number of jobs to be run to follow\nthe executable name on the command line" << endl;
+		}
+	else{
+		if(argv[1][0] == '-') numJobsTotal = atoi(&argv[1][1]);
+		else numJobsTotal = atoi(&argv[1][0]);
+/*		for(int i=1;i<nproc;i++){
+			rc = MPI_SSend(&numJobsTotal, 1, MPI_INT, i, 0, comm);
+			if(rc != MPI_SUCCESS){
+				cout << "Error communicating with process " << i << ".  Terminating." << endl;
+				MPI_Abort(MPI_COMM_WORLD, rc);
+				}
+			}
+*/		}
+
+	}
+ /*  else{
+	MPI_Status stat;
+	rc = MPI_Recv(&numJobsTotal, 1, MPI_INT, 0, MPI_ANY_TAG, comm, &stat);
+	if(rc != MPI_SUCCESS){
+		cout << "Error communicating with process " << 0 << ".  Terminating." << endl;
+		MPI_Abort(MPI_COMM_WORLD, rc);
+		}
+	}
+*/
+  cout << "process " << rank << " about to broadcast" << endl;
+  MPI_Bcast(&numJobsTotal, 1, MPI_INT, 0, comm);
+
+  jobloop(rank,nproc,mycomm,numJobsTotal);
+
+  cout << "process " << rank << " retunred from jobloop" << endl;
+
+  MPI_Barrier(comm);
+  cout << "process " << rank << " passed barrier" << endl;
+  if(rank == 0){
+	char temp[100];
+	for(int i=0;i<numJobsTotal;i++){
+		sprintf(temp, ".lock%d", i);
+		remove(temp);
+		}
+	}
+  
   MPI_Finalize();
-
-  char temp[100];
-  for(int i=0;i<numJobs;i++){
-	sprintf(temp, ".lock%d", i);
-	remove(temp);
-	}
-
   return 0;
 }
 
@@ -61,12 +99,16 @@ void jobloop(int mytid,int ntids,MPI_Comm comm, int numJobs)
 	while(jobNum < numJobs){
 		cout << "process " << mytid << " starting run #" << jobNum << endl;
 		SubGarliMain(jobNum);
-		do{
-			jobNum++;
+		jobNum++;
+		while(jobNum < numJobs){
 			sprintf(temp, ".lock%d", jobNum);
-			}while(FileExists(temp) && (jobNum < numJobs));
-		ofstream lock(temp);
-		lock.close();
+			if(FileExists(temp)) jobNum++;
+			else{
+				ofstream lock(temp);
+				lock.close();
+				break;
+				}
+			}
 		}
 	return;
 }
