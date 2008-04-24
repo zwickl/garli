11c00dad6c69654045ebe1a4c0bb27028067e28e
derrickzwickl <derrickzwickl@f68b6e6a-deb8-11de-9f73-7335da764322>
2008-04-24 18:37:27 +0000

    Added GPL message to all source files

--- garli/trunk/src/funcs.h
+++ garli/trunk/src/funcs.h
@@ -1,18 +1,19 @@
 // GARLI version 0.96b8 source code
-// Copyright  2005-2008 by Derrick J. Zwickl
-// All rights reserved.
+// Copyright 2005-2008 Derrick J. Zwickl
+// email: zwickl@nescent.org
 //
-// This code may be used and modified for non-commercial purposes
-// but redistribution in any form requires written permission.
-// Please contact:
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
 //
-//  Derrick Zwickl
-//	National Evolutionary Synthesis Center
-//	2024 W. Main Street, Suite A200
-//	Durham, NC 27705
-//  email: zwickl@nescent.org
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
 //
-
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #ifndef FUNCS_H
 #define FUNCS_H
