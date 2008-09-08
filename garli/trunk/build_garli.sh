76147a7c3fdab0be29a6dfef3d88128f518c7498
mholder <mholder@f68b6e6a-deb8-11de-9f73-7335da764322>
2008-09-08 17:23:06 +0000

    tweaks to build_garli.sh

--- garli/trunk/build_garli.sh
+++ garli/trunk/build_garli.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
+set -x
 nclv="ncl-2.1.04"
-tar xfvz ${nclv}.tar.gz || exit
+if ! test -d ${nclv}
+then
+	tar xfvz ${nclv}.tar.gz || exit
+fi
 cd ${nclv} || exit
 ./configure --prefix=`pwd`/installed --disable-shared --enable-static --disable-dependency-tracking || exit
 make || exit
 make install || exit
 make installcheck || exit
 cd ..
-./configure --with-ncl=`pwd`/${nclv}/installed || exit
+./configure $@ --with-ncl=`pwd`/${nclv}/installed || exit
 make || exit
