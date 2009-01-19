3e5f357af6c97d7e9a88248b7dbd5034c83b8207
ayres <ayres@f68b6e6a-deb8-11de-9f73-7335da764322>
2009-01-19 19:38:19 +0000

    cuda branch is now merged into trunk

--- garli/branches/cuda/tests/quickTests/subs/sub.internal.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#S -S /bin/bash -cwd
-#$ -o out.internal.$JOB_ID -j y
-#$ -M zwickl@duke.edu
-cd ../internal
-./doRuns ~/bin/working.norm -b
-
-
--- garli/branches/cuda/tests/quickTests/subs/sub.internalConst.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#S -S /bin/bash -cwd
-#$ -o out.internalConst.$JOB_ID -j y
-#$ -M zwickl@duke.edu
-cd ../internalConstraints
-./doRuns ~/bin/working.norm -b
-
-
