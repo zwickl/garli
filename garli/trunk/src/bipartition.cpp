a0807648b90ff42d840ec312d8cbe2e93d2b791f
zwickl <zwickl@f68b6e6a-deb8-11de-9f73-7335da764322>
2009-06-24 19:13:08 +0000

    Finished tweaking bipartition and constraint functions. Removed some supposed improvements from previous commit. Made very significant speed gains for large trees with lots of backbone constraints with a bit of simple reorganization in Tree::AddRandomNodeWithConstraints, Tree::GatherValidReconnectionNodes and Tree::SwapAllowedByConstraint.  Also improved a few things in Bipartition functions, split them reasonably into cpp and h file and purdy'd up the code a bit.

--- garli/trunk/src/bipartition.cpp
+++ garli/trunk/src/bipartition.cpp
@@ -19,17 +19,9 @@
 #include "bipartition.h"
 #include "reconnode.h"
 
-#ifdef BETTER_BIPART
-unsigned Bipartition::nBlocks;
-unsigned Bipartition:: blockBits;
-unsigned Bipartition::ntax;
-#else
 int Bipartition::nBlocks;
 int Bipartition:: blockBits;
 int Bipartition::ntax;
-#endif
-
-//class statics
 unsigned int Bipartition::largestBlockDigit;
 unsigned int Bipartition::allBitsOn;
 char * Bipartition::str;
@@ -52,21 +44,21 @@ void Bipartition::SetBipartitionStatics(int nt){
 	Bipartition::ntax=nt;
 	Bipartition::nBlocks=(int)ceil((FLOAT_TYPE)nt/(FLOAT_TYPE)Bipartition::blockBits);
 	Bipartition::largestBlockDigit=1<<(Bipartition::blockBits-1);
-	Bipartition::allBitsOn=(unsigned int)(pow(2.0, (int) Bipartition::blockBits)-1);
+	Bipartition::allBitsOn=(unsigned int)(pow(2.0, Bipartition::blockBits)-1);
 	Bipartition::str=new char[nt+1];
 	Bipartition::str[nt] = '\0';
 	Bipartition::SetPartialBlockMask();	
 	}
 
 void Bipartition::SetPartialBlockMask(){
-	partialBlockMask=0;
-	unsigned int bit=largestBlockDigit;
-	for(int b=0;b<ntax%blockBits;b++){
-		partialBlockMask += bit;
-		bit = bit >> 1;
+		partialBlockMask=0;
+		unsigned int bit=largestBlockDigit;
+		for(int b=0;b<ntax%blockBits;b++){
+			partialBlockMask += bit;
+			bit = bit >> 1;
+			}
+		if(ntax%blockBits == 0) partialBlockMask=allBitsOn;
 		}
-	if(ntax%blockBits == 0) partialBlockMask=allBitsOn;
-	}
 
 //this function does 2 things
 //1. Fills this bipartition with the bitwise intersection of a backbone mask and a mask
@@ -79,221 +71,54 @@ bool Bipartition::MakeJointMask(const Constraint &constr, const Bipartition *par
 	if(constr.IsBackbone()){
 		//this just uses Bipartition::Operator=()
 		*this = *(constr.GetBackboneMask());
-		if(partialMask != NULL)
+		if(partialMask != NULL)//in this case we'll need to test for meaningful intersection below
 			this->AndEquals(*partialMask);
+		else//here we don't need to check, since a backbone constraint and its own mask must be meaningful
+			return true;
 		}
 	else if(partialMask != NULL){
+		//in this case we'll need to test for meaningful intersection below
 		*this = *(partialMask);
 		}
-	else FillAllBits();
+	else{
+		FillAllBits();
+		return true;
+		}
 
 	Bipartition temp;
 	temp = constr.GetBipartition();
 	temp.AndEquals(*this);
-	if(temp.CountOnBits() < 2) return false;
+	if(temp.MoreThanOneBitSet() == false)
+		return false;
+
 	temp = constr.GetBipartition();
 	temp.Complement();
 	temp.AndEquals(*this);
-	if(temp.CountOnBits() < 2) return false;
-	return true;
-	}
+	if(temp.MoreThanOneBitSet() == false)
+		return false;
 
-
-void Bipartition::ClearBipartition(){
-#ifdef BETTER_BIPART
-	memset(rep, 0L, sizeof(int) * nBlocks);
-#else
-	for(int i=0;i<nBlocks;i++)
-	rep[i]=0;
-#endif
-	}
-
-void Bipartition::operator+=(const Bipartition *rhs){
-	for(int i=0;i<nBlocks;i++){
-		rep[i]|=rhs->rep[i];
-		}
-	}
-
-void Bipartition::operator-=(const Bipartition *rhs){
-	//note that this assumes that rhs is a subset of this or vice versa!!!
-	if(rhs->IsASubsetOf(*this)){
-		for(int i=0;i<nBlocks;i++){
-			rep[i] ^= rhs->rep[i];
-			}
-		}
-	else{
-		for(int i=0;i<nBlocks;i++){
-			rep[i] |= ~rhs->rep[i];
-			}
-		}
-	}
-
-void Bipartition::operator=(const Bipartition *rhs){
-	memcpy(rep, rhs->rep, nBlocks*sizeof(int));
-	}
-
-void Bipartition::operator=(const Bipartition &rhs){
-	memcpy(rep, rhs.rep, nBlocks*sizeof(int));
-	}
-
-int Bipartition::CountOnBits() const{
-	int num=0;
-	for(int i=1;i<=ntax;i++)
-		if(ContainsTaxon(i)) num++;
-	return num;
-	}
-
-void Bipartition::FlipBits(const Bipartition &flip){
-	//this just flips the bits in the passed in bipartition
-	EqualsXORComplement(flip);
-	}
-
-void Bipartition::FillAllBits(){
-#ifdef BETTER_BIPART
-	memset(rep, allBitsOn, sizeof(int) * nBlocks);
-#else
-	for(int i=0;i<nBlocks;i++)
-		rep[i] = allBitsOn;
-#endif
-	}
-
-bool Bipartition::EqualsEquals(const Bipartition &rhs) const{
-	//assert(this->ContainsTaxon(1));
-	//assert(rhs.ContainsTaxon(1));
-	int i;
-	for(i=0;i<nBlocks-1;i++){
-		if(rep[i]!=rhs.rep[i]) return false;
-		}
-	if((rep[i]&partialBlockMask)!=((rhs.rep[i])&partialBlockMask)) return false;
-	return true;
-	}
-
-bool Bipartition::MaskedEqualsEquals(const Bipartition &rhs, const Bipartition &mask) const{
-	//assert(this->ContainsTaxon(1));
-	//assert(rhs.ContainsTaxon(1));
-	int i;
-	for(i=0;i<nBlocks-1;i++){
-		if((rep[i] & mask.rep[i]) != (rhs.rep[i] & mask.rep[i])) return false;
-		}
-	if((rep[i] & partialBlockMask & mask.rep[i]) != ((rhs.rep[i]) & partialBlockMask & mask.rep[i])) return false;
-	return true;
-	}
-
-void Bipartition::Complement(){
-	for(int i=0;i<nBlocks;i++){
-		rep[i] = ~rep[i];
-		}
-	}
-
-bool Bipartition::ComplementEqualsEquals(const Bipartition &rhs) const{
-	int i;
-	for(i=0;i<nBlocks-1;i++){
-		if(~rep[i]!=rhs.rep[i]) return false;
-		}
-		
-	if(((~rep[i])&partialBlockMask)!=(rhs.rep[i]&partialBlockMask)) return false;
-	
 	return true;
 	}
 	
-bool Bipartition::MaskedComplementEqualsEquals(const Bipartition &rhs, const Bipartition &mask) const{
-	//assert(this->ContainsTaxon(1));
-	//assert(rhs.ContainsTaxon(1));
-	int i;
-	for(i=0;i<nBlocks-1;i++){
-		if((~rep[i] & mask.rep[i]) != (rhs.rep[i] & mask.rep[i])) return false;
-		}
-	if((~rep[i] & partialBlockMask & mask.rep[i]) != ((rhs.rep[i]) & partialBlockMask & mask.rep[i])) return false;
-	return true;
-	}
-
-void Bipartition::Standardize(){
-#ifdef BETTER_BIPART
-	if(ContainsFirstTaxon()==true) return;
-#else
-	if(ContainsTaxon(1)==true) return;
-#endif
-	else{
-		for(int i=0;i<nBlocks;i++){
-			rep[i]=~rep[i];
-			}
-		}
-	}
-
-int Bipartition::FirstPresentTaxon() const{
-	int blk=0;
-	unsigned int tmp=rep[blk];
-	while(tmp==0) tmp=rep[++blk];
-	
-	int t=blockBits*blk+1;
-	while( ! (tmp & largestBlockDigit)){
-		tmp = tmp << 1;
-		t++;
-		}
-	return t;
-	}
-
-int Bipartition::FirstNonPresentTaxon() const{
-	int blk=0;
-	unsigned int tmp=rep[blk];
-	while(tmp==allBitsOn) tmp=rep[++blk];
-	
-	int t=blockBits*blk+1;
-	while((tmp & largestBlockDigit)){
-		tmp = tmp << 1;
-		t++;
-		}
-	return t;
-	}
-
-#ifdef BETTER_BIPART
-//if you know the taxon is in the first block, it is uneccesary and very slow to do the divisions
-//in ContainsTaxon
-bool Bipartition::ContainsFirstTaxon() const{
-	if(rep[0] & largestBlockDigit) 
+bool Bipartition::IsCompatibleWithBipartition(const Bipartition &constr) const{
+	//using buneman's 4 point condition.  At least one of the four intersections must be empty
+	//A & B
+	if(HasIntersection(constr, NULL) == false)
+		return true;
+	//A & ~B
+	if(HasIntersectionWithComplement(constr, NULL) == false)
+		return true;
+	//~A & B
+	if(ComplementHasIntersection(constr, NULL) == false)
+		return true;
+	//~A & ~B
+	if(ComplementHasIntersectionWithComplement(constr, NULL) == false)
 		return true;
 	return false;
 	}
 
-bool Bipartition::ContainsTaxon(int t) const{
-	int index = 0;
-	while(((index+1) * blockBits) < t)
-		index++;
-	assert(index < nBlocks);
-	//unsigned int tmp=rep[(t-1)/blockBits];
-	const unsigned int tmp=rep[index];
-	if(tmp & largestBlockDigit >> ((t - 1) - (index * blockBits))) return true;
-	//if(tmp & largestBlockDigit>>((t-1)%blockBits)) return true;
-	return false;
-	}
-#else
-bool Bipartition::ContainsTaxon(int t) const{
-	unsigned int tmp=rep[(t-1)/blockBits];
-	if(tmp & largestBlockDigit>>((t-1)%blockBits)) return true;
-	return false;
-	}
-#endif
 
-void Bipartition::FillWithXORComplement(const Bipartition &lhs, const Bipartition &rhs){
-	for(int i=0;i<nBlocks;i++){
-		rep[i] = lhs.rep[i] ^ (~rhs.rep[i]);
-		}
-	}
-
-void Bipartition::EqualsXORComplement(const Bipartition &rhs){
-	for(int i=0;i<nBlocks;i++){
-		rep[i] = rep[i] ^ (~rhs.rep[i]);
-		}
-	}
-
-void Bipartition::AndEquals(const Bipartition &rhs){
-	//sets the calling bipart to be the bitwise AND of it and the argument
-	for(int i=0;i<nBlocks;i++){
-		rep[i] = rep[i] & rhs.rep[i];
-		}
-	}
-
-bool Bipartition::IsCompatibleWithBipartition(const Bipartition &constr) const{
+bool Bipartition::OldIsCompatibleWithBipartition(const Bipartition &constr) const{
 	//using buneman's 4 point condition.  At least one of the four intersections must be empty
 	bool compat=true;
 	int i;
@@ -339,7 +164,24 @@ bool Bipartition::IsCompatibleWithBipartition(const Bipartition &constr) const{
 	return false;
 	}
 
-bool Bipartition::IsCompatibleWithBipartitionWithMask(const Bipartition &bip, const Bipartition &mask) const{
+bool Bipartition::IsCompatibleWithBipartitionWithMask(const Bipartition &constr, const Bipartition &mask) const{
+	//using buneman's 4 point condition.  At least one of the four intersections must be empty
+	//A & B
+	if(HasIntersection(constr, &mask) == false)
+		return true;
+	//A & ~B
+	if(HasIntersectionWithComplement(constr, &mask) == false)
+		return true;
+	//~A & B
+	if(ComplementHasIntersection(constr, &mask) == false)
+		return true;
+	//~A & ~B
+	if(ComplementHasIntersectionWithComplement(constr, &mask) == false)
+		return true;
+	return false;
+	}
+
+bool Bipartition::OldIsCompatibleWithBipartitionWithMask(const Bipartition &bip, const Bipartition &mask) const{
 	//using buneman's 4 point condition.  At least one of the four intersections must be empty
 	//typically this will be called with 'this' as a POSITIVE constraint
 	bool compat=true;
@@ -347,60 +189,55 @@ bool Bipartition::IsCompatibleWithBipartitionWithMask(const Bipartition &bip, co
 
 	//A & B
 	for(i=0;i<nBlocks-1;i++){
-		if(((rep[i] & bip.rep[i]) & mask.rep[i]) == 0) continue;
+		if(((rep[i] & bip.rep[i]) & mask.rep[i]) == 0) 
+			continue;
 		else{
 			compat=false;
 			break;
 			}
 		}
-	if(compat==true && ((((rep[i] & bip.rep[i]) & mask.rep[i]) & partialBlockMask) == 0)) return true;
+	if(compat==true && ((((rep[i] & bip.rep[i]) & mask.rep[i]) & partialBlockMask) == 0)) 
+		return true;
 	//A & ~B
 	compat=true;
 	for(i=0;i<nBlocks-1;i++){
-		if(((rep[i] & ~bip.rep[i]) & mask.rep[i]) == 0) continue;
+		if(((rep[i] & ~bip.rep[i]) & mask.rep[i]) == 0) 
+			continue;
 		else{
 			compat=false;
 			break;
 			}
 		}
-	if(compat==true && ((((rep[i] & ~bip.rep[i]) & mask.rep[i]) & partialBlockMask) == 0)) return true;
+	if(compat==true && ((((rep[i] & ~bip.rep[i]) & mask.rep[i]) & partialBlockMask) == 0)) 
+		return true;
 	//~A & B
 	compat=true;
 	for(i=0;i<nBlocks-1;i++){
-		if(((~rep[i] & bip.rep[i]) & mask.rep[i]) == 0) continue;
+		if(((~rep[i] & bip.rep[i]) & mask.rep[i]) == 0) 
+			continue;
 		else{
 			compat=false;
 			break;
 			}
 		}
-	if(compat==true && ((((~rep[i] & bip.rep[i]) & mask.rep[i]) & partialBlockMask) == 0)) return true;
+	if(compat==true && ((((~rep[i] & bip.rep[i]) & mask.rep[i]) & partialBlockMask) == 0)) 
+		return true;
 	//~A & ~B
 	compat=true;
 	for(i=0;i<nBlocks-1;i++){
-		if(((~rep[i] & ~bip.rep[i]) & mask.rep[i]) == 0) continue;
+		if(((~rep[i] & ~bip.rep[i]) & mask.rep[i]) == 0) 
+			continue;
 		else{
 			compat=false;
 			break;
 			}
 		}
-	if(compat==true && ((((~rep[i] & ~bip.rep[i]) & mask.rep[i]) & partialBlockMask) == 0)) return true;
+	if(compat==true && ((((~rep[i] & ~bip.rep[i]) & mask.rep[i]) & partialBlockMask) == 0)) 
+		return true;
 	return false;
 	}
 
-bool Bipartition::IsCompatibleWithNegativeBipartition(const Bipartition &bip) const{
-	//To be consistent with a negative bipartition (constraint) neither the bipartition
-	//or its complement can BE the constraint bipartition
-	if(this->EqualsEquals(bip)==false && this->ComplementEqualsEquals(bip)==false) return true;
-	else return false;
-	}
-
-bool Bipartition::IsCompatibleWithNegativeBipartitionWithMask(const Bipartition &bip, const Bipartition &mask) const{
-	//To be consistent with a negative bipartition (constraint) neither the masked bipartition
-	//or its masked complement can BE the constraint bipartition
-	if(this->MaskedEqualsEquals(bip, mask)==false && this->MaskedComplementEqualsEquals(bip, mask)==false) return true;
-	else return false;
-	}
-
+//This isn't used currently and may not be working
 bool Bipartition::IsIncompatibleWithBipartitionWithMask(const Bipartition &bip, const Bipartition &mask) const{
 	//using buneman's 4 point condition.  At none of the four intersections must be empty
 	//typically this will be called with 'this' as a NEGATIVE constraint
@@ -476,56 +313,6 @@ bool Bipartition::IsIncompatibleWithBipartitionWithMask(const Bipartition &bip, 
 	return true;
 	}
 
-bool Bipartition::IsASubsetOf(const Bipartition &target) const{
-	//returns true if this is a subset of target
-	int i;
-	for(i=0;i<nBlocks-1;i++){
-		if((target.rep[i] | rep[i]) != target.rep[i]) return false;
-		}
-	if(((target.rep[i] | rep[i]) & partialBlockMask) != (target.rep[i] & partialBlockMask)) return false;
-	return true;
-	}
-
-bool Bipartition::MaskedIsASubsetOf(const Bipartition &target, const Bipartition &mask) const{
-	//returns true if this is a subset of target
-	int i;
-	for(i=0;i<nBlocks-1;i++){
-		if(((target.rep[i] | rep[i]) & mask.rep[i]) != (target.rep[i] & mask.rep[i])) return false;
-		}
-	if(((target.rep[i] | rep[i]) & partialBlockMask & mask.rep[i]) != (target.rep[i] & partialBlockMask & mask.rep[i])) return false;
-	return true;
-	}
-
-bool Bipartition::ComplementIsASubsetOf(const Bipartition &target) const{
-	//returns true if this is NOT a subset of target's complement
-	int i;
-	for(i=0;i<nBlocks-1;i++){
-		if((target.rep[i] | ~rep[i]) != ~rep[i]) return true;
-		}
-	if(((target.rep[i] | ~rep[i]) & partialBlockMask) != (~rep[i] & partialBlockMask)) return false;
-	return true;
-	}
-
-Bipartition * Bipartition::TerminalBipart(int taxNum){
-	assert(taxNum > 0 && taxNum <= ntax);
-	for(int i=0;i<nBlocks;i++) rep[i]=0;
-	//8-19-05 fixed this bit of stupidity
-	//rep[(taxNum)/blockBits]|=(largestBlockDigit>>((taxNum-1)%blockBits));
-	rep[(taxNum-1)/blockBits]|=(largestBlockDigit>>((taxNum-1)%blockBits));
-	return this;
-	}
-char * Bipartition::Output(){
-	for(int i=0;i<nBlocks;i++){
-		unsigned int t=rep[i];
-		for(int j=0;j<blockBits;j++){
-			if(i*blockBits+j >= ntax) break;
-			if(t&largestBlockDigit) str[i*blockBits+j]='*';
-			else str[i*blockBits+j]='-';
-			t=t<<1;
-			}
-		}
-	return str;
-	}
 
 void Bipartition::NumericalOutput(string &out, const Bipartition *mask){
 	string left = "(";
@@ -577,37 +364,4 @@ void Bipartition::NumericalOutput(string &out, const Bipartition *mask){
 	left += ')';
 	right += ')';
 	out = left + " | " + right;
-	}
-/*
-void BinaryOutput(ofstream &out){
-	int size = nBlocks * sizeof(unsigned int);
-	out.write((char*) rep, size);
-	}
-*/
-void Bipartition::BinaryOutput(OUTPUT_CLASS &out){
-	out.WRITE_TO_FILE(rep, sizeof(unsigned int), nBlocks);
-	}
-
-void Bipartition::BinaryInput(FILE* &in){
-	fread((char*) rep, sizeof(unsigned int), nBlocks, in);
-	}
-
-vector<int> Bipartition::NodenumsFromBipart(){
-	vector<int> nodes;
-	for(int i=1;i<=ntax;i++) if(ContainsTaxon(i)) nodes.push_back(i);
-	return nodes; 
-	}
-
-void Bipartition::BipartFromNodenums(const vector<int> & nodes){
-	ClearBipartition();
-	Bipartition temp;
-	for(vector<int>::const_iterator it = nodes.begin();it != nodes.end();it++)
-		*this += temp.TerminalBipart(*it);
-	}
-
-void Bipartition::BipartFromNodenums(const std::set<unsigned> & nodes){
-	ClearBipartition();
-	Bipartition temp;
-	for(set<unsigned>::const_iterator it = nodes.begin();it != nodes.end();it++)
-		*this += temp.TerminalBipart(*it);
 	}
