730fc7b5446becb26bcb64c825848ac8e64cce17
zwickl <zwickl@f68b6e6a-deb8-11de-9f73-7335da764322>
2009-08-21 19:56:55 +0000

    Silly header thing.

--- garli/trunk/src/tree.h
+++ garli/trunk/src/tree.h
@@ -264,7 +264,7 @@ class Tree{
 #ifdef SINGLE_PRECISION_FLOATS
 		FLOAT_TYPE Tree::OptimizeBoundedParameter(FLOAT_TYPE optPrecision, FLOAT_TYPE initialVal, int which, FLOAT_TYPE lowBound, FLOAT_TYPE highBound, void (Model::*SetParam)(int, FLOAT_TYPE), FLOAT_TYPE targetScoreDigits = 5.0f);
 #else
-		FLOAT_TYPE Tree::OptimizeBoundedParameter(FLOAT_TYPE optPrecision, FLOAT_TYPE initialVal, int which, FLOAT_TYPE lowBound, FLOAT_TYPE highBound, void (Model::*SetParam)(int, FLOAT_TYPE), FLOAT_TYPE targetScoreDigits = 9.0);
+		FLOAT_TYPE OptimizeBoundedParameter(FLOAT_TYPE optPrecision, FLOAT_TYPE initialVal, int which, FLOAT_TYPE lowBound, FLOAT_TYPE highBound, void (Model::*SetParam)(int, FLOAT_TYPE), FLOAT_TYPE targetScoreDigits = 9.0);
 #endif
 		//FLOAT_TYPE OptimizeBoundedParameter(FLOAT_TYPE optPrecision, FLOAT_TYPE prevVal, int which, FLOAT_TYPE lowBound, FLOAT_TYPE highBound, void (Model::*SetParam)(int, FLOAT_TYPE));
 		FLOAT_TYPE SetAndEvaluateParameter(int which, FLOAT_TYPE val, void (Model::*SetParam)(int, FLOAT_TYPE));
