2947a545e5ca7abaa6e4f5fbedb8937b9c14215e
dzwickl <dzwickl@f68b6e6a-deb8-11de-9f73-7335da764322>
2010-08-20 01:14:52 +0000

    arg, as with previous commits

--- garli/branches/partitioning/src/population.cpp
+++ garli/branches/partitioning/src/population.cpp
@@ -1760,11 +1760,11 @@ void Population::Run(){
 							improve += bestTree->OptimizeFlexRates(adap->branchOptPrecision, modnum);
 							}
 						else if(modSpec->fixAlpha == false){//normal gamma
-							improve += bestTree->OptimizeBoundedParameter(adap->branchOptPrecision, mod->Alpha(), 0, min(mod->Alpha(), 0.05), 999.9, modnum, &Model::SetAlpha);
+							improve += bestTree->OptimizeBoundedParameter(modnum, adap->branchOptPrecision, mod->Alpha(), 0, min(mod->Alpha(), 0.05), 999.9, &Model::SetAlpha);
 							}
 						}
 					if(modSpec->includeInvariantSites && !modSpec->fixInvariantSites)
-						improve += bestTree->OptimizeBoundedParameter(adap->branchOptPrecision, mod->PropInvar(), 0, min(mod->PropInvar(), 1.0e-8), mod->maxPropInvar, modnum, &Model::SetPinv);
+						improve += bestTree->OptimizeBoundedParameter(modnum, adap->branchOptPrecision, mod->PropInvar(), 0, min(mod->PropInvar(), 1.0e-8), mod->maxPropInvar, &Model::SetPinv);
 #ifdef MORE_DETERM_OPT
 					if(modSpec->IsCodon() == false && modSpec->fixStateFreqs == false && modSpec->IsEqualStateFrequencies() == false && modSpec->IsEmpiricalStateFrequencies() == false){
 						FLOAT_TYPE paramOpt = bestTree->OptimizeEquilibriumFreqs(adap->branchOptPrecision, modnum);
