7ded3ecc02785bd3ffc6f5101ecbcb971c5edfc3
dzwickl <dzwickl@f68b6e6a-deb8-11de-9f73-7335da764322>
2011-01-28 15:23:27 +0000

    output changes

--- garli/branches/partitioning/src/model.cpp
+++ garli/branches/partitioning/src/model.cpp
@@ -2207,8 +2207,8 @@ void Model::OutputHumanReadableModelReportWithParams() const{
 	
 	if(modSpec->IsNucleotide() || modSpec->IsCodon()){
 		if(modSpec->IsCodon() && modSpec->numRateCats == 1) outman.UserMessageNoCR("  One estimated dN/dS ratio (aka omega) = %f\n", Omega(0));
-		if(modSpec->IsCodon()) outman.UserMessage("  Nucleotide Relative Rate Matrix Assumed by Codon Model:     ");
-		else outman.UserMessage("  Nucleotide Relative Rate Matrix: ");
+		if(modSpec->IsCodon()) outman.UserMessageNoCR("  Nucleotide Relative Rate Matrix Assumed by Codon Model: ");
+		else outman.UserMessageNoCR("  Nucleotide Relative Rate Matrix: ");
 		if(nst == 6){
 			if(modSpec->IsArbitraryRateMatrix()) outman.UserMessage("\n    User specified matrix type: %s ", modSpec->arbitraryRateMatrixString.c_str());
 			else outman.UserMessage("    6 rates ");
@@ -4390,6 +4390,7 @@ void ModelPartition::ReadGarliFormattedModelStrings(string &modstr){
 		outman.UserMessage("\nProper format for specification of model parameters in the partitioned\nversion is as follows. Neither subset rates nor all models are required to\nappear. Line breaks are ignored, but the string must be terminated with a \";\".\nThe first model is M0. Omit the <>'s in the following.");
 		outman.UserMessage("\n\nS <subset rate 0> <subset rate 1> <etc.>\nM<first model number> <garli formatted param string for model>\nM<second model number>  <garli formatted param string for model>\n <etc.> ;");
 		outman.UserMessage("\nExample for 3 models:\nS  0.551458  0.302705  2.145837\nM0 r 1.959444 2.571568 1.406484 1.406484 3.725263 e 0.310294 0.176855 0.297080 0.215771 a 0.410964\nM1 r 4.366321 7.061605 1.603498 7.061605 4.366321 e 0.269302 0.163670 0.160508 0.406520 a 0.361294\nM2 r 1.000000 4.908101 3.372480 0.457829 4.908101 e 0.156505 0.353697 0.287843 0.201954 a 4.098323 p 0.034152;");
+		outman.UserMessage("\nWhen there is only one model (i.e., unpartitioned analyses), the \"M0\" part\nthat indicates the model number need not appear.");
 		throw mess;
 		}
 	}
